174f8e2f4af473467186a7096e2ee51fec7456a0
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-04-17 18:06:04 +0200

    chore: update dependencies, remove unused ones

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,7 @@ module.exports = {
     `@babel/preset-typescript`,
   ],
   plugins: [
-    [`@babel/plugin-proposal-decorators`, {legacy: true}],
-    [`@babel/plugin-proposal-class-properties`, {loose: true}],
     [`@babel/plugin-transform-modules-commonjs`],
-    [`@babel/plugin-proposal-nullish-coalescing-operator`],
     [`babel-plugin-dynamic-import-node`],
   ],
 };
--- sources/fsUtils.ts
+++ sources/fsUtils.ts
@@ -1,24 +1,5 @@
-import fs from 'fs';
+import { rm } from "fs/promises";
 
 export async function rimraf(path: string) {
-  const [major, minor] = process.versions.node.split(`.`).map(section => Number(section));
-
-  if (major > 14 || (major === 14 && minor >= 14)) {
-    // rm was added in v14.14.0
-    return fs.promises.rm(path, {recursive: true});
-  } else if (major > 12 || (major === 12 && minor >= 10)) {
-    // rmdir got support for recursive in v12.10.0 and was deprecated in v14.14.0
-    return fs.promises.rmdir(path, {recursive: true});
-  } else {
-    const rimraf = await import(`rimraf`);
-    return new Promise<void>((resolve, reject) => {
-      rimraf.default(path, err => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
-  }
+  return rm(path, { recursive: true, force: true });
 }
