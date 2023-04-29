a0ff7e8265920f0b96b919f81ab453c9d1ae4df0
tison <wander4096@gmail.com>
2023-04-29 22:54:47 +0800

    fix typecheck

--- sources/httpUtils.ts
+++ sources/httpUtils.ts
@@ -15,10 +15,10 @@ export async function fetchUrlStream(url: string, options: RequestOptions = {}) 
   return new Promise<IncomingMessage>((resolve, reject) => {
     const request = https.get(url, {...options, agent: proxyAgent}, response => {
       const statusCode = response.statusCode;
-      if (!(statusCode >= 200 && statusCode < 300))
-        return reject(new Error(`Server answered with HTTP ${statusCode} when performing the request to ${url}; for troubleshooting help, see https://github.com/nodejs/corepack#troubleshooting`));
+      if (statusCode && statusCode >= 200 && statusCode < 300)
+        return resolve(response);
 
-      return resolve(response);
+      return reject(new Error(`Server answered with HTTP ${statusCode} when performing the request to ${url}; for troubleshooting help, see https://github.com/nodejs/corepack#troubleshooting`));
     });
 
     request.on(`error`, err => {
