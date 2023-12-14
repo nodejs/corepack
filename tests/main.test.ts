b994028ae2c364c7fb2abe871ca15344f28bbe9c
elrrrrrrr <elrrrrrrr@gmail.com>
2023-12-14 21:42:06 +0800

    Update tests/main.test.ts
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -80,6 +80,8 @@ const testedPackageManagers: Array<[string, string]> = [
   [`pnpm`, `6.6.2+sha1.7b4d6b176c1b93b5670ed94c24babb7d80c13854`],
   [`pnpm`, `6.6.2+sha224.eb5c0acad3b0f40ecdaa2db9aa5a73134ad256e17e22d1419a2ab073`],
   [`cnpm`, `9.3.2`],
+  [`cnpm`, `9.3.2+sha1.05a307b7c1d34a368b8d30ff159dfd0533c8c328`],
+  [`cnpm`, `9.3.2+sha224.6d124ff3ac4ea4ba18833993e063fd8091156d6400ed7a2e5ad47ce3`],
   [`npm`, `6.14.2`],
   [`npm`, `6.14.2+sha1.f057d35cd4792c4c511bb1fa332edb43143d07b0`],
   [`npm`, `6.14.2+sha224.50512c1eb404900ee78586faa6d756b8d867ff46a328e6fb4cdf3a87`],
