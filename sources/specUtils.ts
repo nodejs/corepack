9bee4150815113d97f0bd77d62c8d999cfd68ad3
Maël Nison <nison.mael@gmail.com>
2023-12-29 13:00:38 +0100

    feat: add support for rangeless commands (#338)
    This diff makes it possible to run the CLI commands without an explicit range, in which case we use an implicit `*`, e.g.: `corepack install -g yarn`.

--- sources/specUtils.ts
+++ sources/specUtils.ts
@@ -11,16 +11,16 @@ export function parseSpec(raw: unknown, source: string, {enforceExactVersion = t
   if (typeof raw !== `string`)
     throw new UsageError(`Invalid package manager specification in ${source}; expected a string`);
 
-  const match = raw.match(/^(?!_)(.+)@(.+)$/);
-  if (match === null || (enforceExactVersion && !semver.valid(match[2])))
-    throw new UsageError(`Invalid package manager specification in ${source}; expected a semver version${enforceExactVersion ? `` : `, range, or tag`}`);
+  const match = raw.match(/^(?!_)([^@]+)(?:@(.+))?$/);
+  if (match === null || (enforceExactVersion && (!match[2] || !semver.valid(match[2]))))
+    throw new UsageError(`Invalid package manager specification in ${source} (${raw}); expected a semver version${enforceExactVersion ? `` : `, range, or tag`}`);
 
   if (!isSupportedPackageManager(match[1]))
     throw new UsageError(`Unsupported package manager specification (${match})`);
 
   return {
     name: match[1],
-    range: match[2],
+    range: match[2] ?? `*`,
   };
 }
 
@@ -57,7 +57,7 @@ export async function findProjectSpec(initialCwd: string, locator: Locator, {tra
       case `NoProject`:
       case `NoSpec`: {
         return fallbackLocator;
-      } break;
+      }
 
       case `Found`: {
         if (result.spec.name !== locator.name) {
@@ -69,7 +69,7 @@ export async function findProjectSpec(initialCwd: string, locator: Locator, {tra
         } else {
           return result.spec;
         }
-      } break;
+      }
     }
   }
 }
