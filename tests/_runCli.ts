811825885f718f5cc350c421e168a39abeb45152
merceyz <merceyz@users.noreply.github.com>
2024-01-28 15:07:06 +0100

    chore: remove old request recording

--- tests/_runCli.ts
+++ tests/_runCli.ts
@@ -8,7 +8,7 @@ export async function runCli(cwd: PortablePath, argv: Array<string>): Promise<{e
   return new Promise((resolve, reject) => {
     if (process.env.RUN_CLI_ID)
       (process.env.RUN_CLI_ID as any)++;
-    const child = spawn(process.execPath, [`--no-warnings`, `-r`, require.resolve(`./recordRequests.js`), require.resolve(`../dist/corepack.js`), ...argv], {
+    const child = spawn(process.execPath, [`--no-warnings`, require.resolve(`../dist/corepack.js`), ...argv], {
       cwd: npath.fromPortablePath(cwd),
       env: process.env,
       stdio: `pipe`,
