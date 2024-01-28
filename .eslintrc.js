d67d7f35eed8ea0b2fae09ceccea5f8705c78c25
merceyz <merceyz@users.noreply.github.com>
2024-01-28 15:07:06 +0100

    chore: add lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,11 @@ module.exports = {
   extends: [
     `@yarnpkg`,
   ],
+  rules: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    'no-restricted-globals': [`error`, {
+      name: `fetch`,
+      message: `Use fetch from sources/fetchUtils.ts`,
+    }],
+  },
 };
