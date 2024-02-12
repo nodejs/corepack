6541c8af0394b513c6270ba19f1e11a768eda0a8
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-02-12 23:23:58 +0100

    move removal of `COREPACK_` env variables

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -6,25 +6,25 @@ const process = require(`process`);
 jest.retryTimes(2, {logErrorsBeforeRetry: true});
 
 const OLD_ENV = process.env;
+const {
+  // To ensure we test the default behavior, we must remove these env vars
+  // in case the local machine already set these values.
+  COREPACK_DEFAULT_TO_LATEST,
+  COREPACK_ENABLE_NETWORK,
+  COREPACK_ENABLE_PROJECT_SPEC,
+  COREPACK_ENABLE_STRICT,
+  COREPACK_HOME,
+  COREPACK_NPM_REGISTRY,
+  COREPACK_NPM_TOKEN,
+  COREPACK_NPM_USERNAME,
+  FORCE_COLOR,
+  // We save the rest to put it into `process.env` for tests.
+  ...processEnv
+} = process.env;
 
 switch (process.env.NOCK_ENV || ``) {
   case `record`:
-  case `replay`: {
-    const {
-      // To ensure we test the default behavior, we must remove these env vars
-      // in case the local machine already set these values.
-      COREPACK_DEFAULT_TO_LATEST,
-      COREPACK_ENABLE_NETWORK,
-      COREPACK_ENABLE_PROJECT_SPEC,
-      COREPACK_ENABLE_STRICT,
-      COREPACK_HOME,
-      COREPACK_NPM_REGISTRY,
-      COREPACK_NPM_TOKEN,
-      COREPACK_NPM_USERNAME,
-      FORCE_COLOR,
-      // We save the rest to put it into `process.env` for tests.
-      ...processEnv
-    } = process.env;
+  case `replay`:
     beforeEach(() => {
       process.env = {
         ...processEnv,
@@ -35,11 +35,11 @@ switch (process.env.NOCK_ENV || ``) {
           .digest(`base64url`),
       };
     });
-  } break;
+    break;
 
   case ``: {
     beforeEach(() => {
-      process.env = {...OLD_ENV};
+      process.env = {...processEnv};
     });
   } break;
 
