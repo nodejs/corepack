6c9ea22c3edf3036789416ff1c59106a8b00ef02
merceyz <merceyz@users.noreply.github.com>
2024-02-21 20:08:34 +0100

    test: clear all `COREPACK_*` environment variables

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -6,21 +6,13 @@ const process = require(`process`);
 jest.retryTimes(2, {logErrorsBeforeRetry: true});
 
 const OLD_ENV = process.env;
-const {
-  // To ensure we test the default behavior, we must remove these env vars
-  // in case the local machine already set these values.
-  COREPACK_DEFAULT_TO_LATEST,
-  COREPACK_ENABLE_NETWORK,
-  COREPACK_ENABLE_PROJECT_SPEC,
-  COREPACK_ENABLE_STRICT,
-  COREPACK_HOME,
-  COREPACK_NPM_REGISTRY,
-  COREPACK_NPM_TOKEN,
-  COREPACK_NPM_USERNAME,
-  FORCE_COLOR,
-  // We save the rest to put it into `process.env` for tests.
-  ...processEnv
-} = process.env;
+// To ensure we test the default behavior, we must remove these env vars
+// in case the local machine already set these values.
+const processEnv = Object.fromEntries(
+  Object.keys(process.env)
+    .filter(key => key !== `FORCE_COLOR` && !key.startsWith(`COREPACK_`))
+    .map(key => [key, process.env[key]]),
+);
 
 switch (process.env.NOCK_ENV || ``) {
   case `record`:
