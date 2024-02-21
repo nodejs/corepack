00506b2a15dd87ec03240578077a35b7980e00c1
Maël Nison <nison.mael@gmail.com>
2024-02-21 21:24:13 +0100

    fix: group the download prompt together (#391)

--- sources/httpUtils.ts
+++ sources/httpUtils.ts
@@ -40,18 +40,19 @@ export async function fetchAsJson(input: string | URL, init?: RequestInit) {
 
 export async function fetchUrlStream(input: string | URL, init?: RequestInit) {
   if (process.env.COREPACK_ENABLE_DOWNLOAD_PROMPT === `1`) {
-    console.error(`Corepack is about to download ${input}.`);
+    console.error(`Corepack is about to download ${input}`);
     if (stdin.isTTY && !process.env.CI) {
-      stderr.write(`\nDo you want to continue? [Y/n] `);
+      stderr.write(`Do you want to continue? [Y/n] `);
       stdin.resume();
       const chars = await once(stdin, `data`);
       stdin.pause();
-      if (
-        chars[0][0] === 0x6e || // n
-        chars[0][0] === 0x4e // N
-      ) {
+
+      // n / N
+      if (chars[0][0] === 0x6e || chars[0][0] === 0x4e)
         throw new UsageError(`Aborted by the user`);
-      }
+
+      // Add a newline to separate Corepack output from the package manager
+      console.error();
     }
   }
 
