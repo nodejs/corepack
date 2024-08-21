e2d65672199f215b1d5f3d0ab30b156c575db45b
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-08-21 14:27:09 -0400

    add tests

--- tests/Project.test.ts
+++ tests/Project.test.ts
@@ -1,9 +1,75 @@
-import {describe, it} from 'vitest';
+import {ppath, xfs, npath}                from '@yarnpkg/fslib';
+import process                            from 'node:process';
+import {describe, beforeEach, it, expect} from 'vitest';
+
+import {runCli}                           from './_runCli';
+
+beforeEach(async () => {
+  // `process.env` is reset after each tests in setupTests.js.
+  process.env.COREPACK_HOME = npath.fromPortablePath(await xfs.mktempPromise());
+  process.env.COREPACK_DEFAULT_TO_LATEST = `0`;
+});
 
 describe(`ProjectCommand`, () => {
   describe(`InstallSubcommand`, () => {
-    it(`should add the binaries in the folder found in the PATH`, async () => {
-      // todo
+    it(`should install with npm`, async () => {
+      await xfs.mktempPromise(async cwd => {
+        await xfs.writeJsonPromise(ppath.join(cwd, `package.json`), {
+          packageManager: `npm@10.8.2`,
+          dependencies: {
+            ms: `2.1.3`,
+          },
+        });
+
+        await expect(runCli(cwd, [`project`, `install`])).resolves.toMatchObject({
+          exitCode: 0,
+          stderr: ``,
+        });
+
+        const dir = await xfs.readdirPromise(cwd);
+        expect(dir).toContain(`package-lock.json`);
+        expect(dir).toContain(`node_modules`);
+      });
+    });
+
+    it(`should install with pnpm`, async () => {
+      await xfs.mktempPromise(async cwd => {
+        await xfs.writeJsonPromise(ppath.join(cwd, `package.json`), {
+          packageManager: `pnpm@9.4.0`,
+          dependencies: {
+            ms: `2.1.3`,
+          },
+        });
+
+        await expect(runCli(cwd, [`project`, `install`])).resolves.toMatchObject({
+          exitCode: 0,
+          stderr: ``,
+        });
+
+        const dir = await xfs.readdirPromise(cwd);
+        expect(dir).toContain(`pnpm-lock.yaml`);
+        expect(dir).toContain(`node_modules`);
+      });
+    });
+
+    it(`should install with yarn`, async () => {
+      await xfs.mktempPromise(async cwd => {
+        await xfs.writeJsonPromise(ppath.join(cwd, `package.json`), {
+          packageManager: `yarn@2.1.0`,
+          dependencies: {
+            ms: `2.1.3`,
+          },
+        });
+
+        await expect(runCli(cwd, [`project`, `install`])).resolves.toMatchObject({
+          exitCode: 0,
+          stderr: ``,
+        });
+
+        const dir = await xfs.readdirPromise(cwd);
+        expect(dir).toContain(`yarn.lock`);
+        expect(dir).toContain(`.pnp.js`);
+      });
     });
   });
 });
